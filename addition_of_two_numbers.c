7d7253a4dd3f750e505bc4cd05ee638ce0956d55
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-08-10 15:06:16 +0530

    Added C programs

---
+++ addition_of_two_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+
+    
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    sum = num1 + num2;
+    printf("The sum of two numbers is %d", sum);
+
+    return 0;
+}
---
+++ area_of_rectangle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float length, width, area;
+
+    printf("Enter length: ");
+    scanf("%f", &length);
+
+    printf("Enter width: ");
+    scanf("%f", &width);
+
+    area = length * width;
+
+    printf("Area of rectangle = %.2f", area);
+
+    return 0;
+}
+
