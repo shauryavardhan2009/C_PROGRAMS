f9343081d36458b55a4da5764c1c78bfe1af2377
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-08-18 16:04:32 +0530

    commit

---
+++ 3_points_collinearity.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int x1, y1, x2, y2, x3, y3;
+    printf("Enter the coordinates of the first point (x1, y1): ");
+    scanf("%d %d", &x1, &y1);
+    printf("Enter the coordinates of the second point (x2, y2): ");
+    scanf("%d %d", &x2, &y2);
+    printf("Enter the coordinates of the third point (x3, y3): ");
+    scanf("%d %d", &x3, &y3);
+    
+
+
+}
