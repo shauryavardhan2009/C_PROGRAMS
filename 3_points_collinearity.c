04f884bf2fa0f850ab41ca00e35d9f9071636f62
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-08-18 16:26:46 +0530

    commit

--- 3_points_collinearity.c
+++ 3_points_collinearity.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 int main() {
     int x1, y1, x2, y2, x3, y3;
-    printf("Enter the coordinates of the first point (x1, y1): ");
+
+    printf("Enter coordinates of first point (x1 y1): ");
     scanf("%d %d", &x1, &y1);
-    printf("Enter the coordinates of the second point (x2, y2): ");
+
+    printf("Enter coordinates of second point (x2 y2): ");
     scanf("%d %d", &x2, &y2);
-    printf("Enter the coordinates of the third point (x3, y3): ");
+
+    printf("Enter coordinates of third point (x3 y3): ");
     scanf("%d %d", &x3, &y3);
-    
 
+    if ((x2 - x1) * (y3 - y1) == (y2 - y1) * (x3 - x1))
+        printf("The three points are collinear.\n");
+    else
+        printf("The three points are not collinear.\n");
 
+    return 0;
 }
