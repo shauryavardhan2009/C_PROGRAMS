1740bf1ae1f7e6c4a37b13aa93572626d852c02a
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-01 21:56:50 +0530

    commit

--- pattern6.c
+++ pattern6.c
@@ -6,3 +6,27 @@
    *
 
  */
+#include <stdio.h>
+
+int main()
+{
+    int i, j, space = 0;
+
+    for (i = 7; i >= 1; i -= 2)
+    {
+        for (j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        space++;
+    }
+
+    return 0;
+}
