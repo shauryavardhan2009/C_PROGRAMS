81b30b012c9a4fcebe97c733d30a3cb8db5764f5
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-01 22:07:12 +0530

    commit

--- pattern8.c
+++ pattern8.c
@@ -1,8 +1,34 @@
 /* 
  Write a program to print the following pattern:
- 
+
 1234567
  12345
   123
    1  
  */
+
+ #include <stdio.h>
+
+int main()
+{
+    int i, j, space;
+
+    for(i = 7; i >= 1; i -= 2)
+    {
+        // Print leading spaces
+        for(space = 1; space <= (7 - i) / 2; space++)
+        {
+            printf(" ");
+        }
+
+        // Print numbers
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
