ddefed9cddacced4e68cb89e5b96dfaa26843653
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-01 22:00:55 +0530

    commit

--- pattern5.c
+++ pattern5.c
@@ -9,4 +9,20 @@
 
  */
 
- 
+ #include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 1; i <= 5; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
