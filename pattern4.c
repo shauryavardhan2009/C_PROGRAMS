1ed0955f2e9712d5f04f6f7b3bffa83cb84bf13c
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-01 22:09:33 +0530

    commit

--- pattern4.c
+++ pattern4.c
@@ -6,5 +6,30 @@
  *****
 *******
  */
+#include <stdio.h>
+
+int main()
+{
+    int i, j, space;
+
+    for(i = 1; i <= 4; i++)
+    {
+        // Print leading spaces
+        for(space = 1; space <= 4 - i; space++)
+        {
+            printf(" ");
+        }
+
+        // Print stars
+        for(j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
 
  
