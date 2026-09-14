2b450c7b034bfd518985fe9f86c0692c0d146a9b
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 22:19:33 +0530

    commit

--- searching_element_in_array.c
+++ searching_element_in_array.c
@@ -1,1 +1,38 @@
 // Search for an element in an array using linear search. 
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i, search, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &search);
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == search)
+        {
+            printf("Element found at position %d\n", i + 1);
+            found = 1;
+            break;
+        }
+    }
+
+    if(found == 0)
+    {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
