bf86ca83ad5f12692f1ac0fe5f28c9ebbc6e6a88
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 21:55:05 +0530

    commit

--- evenodd_in_array.c
+++ evenodd_in_array.c
@@ -1,1 +1,32 @@
 // count even and odd numbers in an array. 
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+    int even = 0, odd = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    printf("Even numbers = %d\n", even);
+    printf("Odd numbers = %d\n", odd);
+
+    return 0;
+}
