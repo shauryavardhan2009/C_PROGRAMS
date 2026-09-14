5bcca1fb6eebe3267542f50b2c605430b18c5f6f
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 21:50:59 +0530

    commit

--- 2nd_largest_element_array.c
+++ 2nd_largest_element_array.c
@@ -1,1 +1,47 @@
 //Find the second largest element in an array.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, largest, second;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    largest = a[0];
+    second = a[1];
+
+    if(second > largest)
+    {
+        int temp = largest;
+        largest = second;
+        second = temp;
+    }
+
+    for(i = 2; i < n; i++)
+    {
+        if(a[i] > largest)
+        {
+            second = largest;
+            largest = a[i];
+        }
+        else if(a[i] > second && a[i] != largest)
+        {
+            second = a[i];
+        }
+    }
+
+    printf("Second largest element = %d", second);
+
+    return 0;
+}
