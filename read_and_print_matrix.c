f194968e2f358faa4135ad195fc9386b80789afd
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 22:16:45 +0530

    commit

--- read_and_print_matrix.c
+++ read_and_print_matrix.c
@@ -1,1 +1,34 @@
 //Read and print a matrix.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100][100], m, n, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &m);
+    printf("Enter number of columns: ");
+    scanf("%d", &n);
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < m; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Elements of the matrix are:\n");
+    for(i = 0; i < m; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
