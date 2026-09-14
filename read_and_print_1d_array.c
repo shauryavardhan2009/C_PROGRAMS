dfac15e17b332493f2cdcdb5b513b007bc4328cf
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 22:15:20 +0530

    commit

--- read_and_print_1d_array.c
+++ read_and_print_1d_array.c
@@ -1,2 +1,26 @@
 // Read and print elements of a one-dimensional array.
 #include <stdio.h>
+
+int main()
+{
+    int a[100], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Elements of the array are:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
