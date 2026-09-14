25fd484edc9a7023967dff870fa92b4d484da0cd
Shaurya Vardhan Singh <singh09shaurya@gmail.com>
2026-09-14 21:52:56 +0530

    commit

--- delete_form_array.c
+++ delete_form_array.c
@@ -1,1 +1,44 @@
 //Delete an element from an array
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, pos, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Enter the position to delete: ");
+    scanf("%d", &pos);
+
+    if(pos < 1 || pos > n)
+    {
+        printf("Invalid position");
+    }
+    else
+    {
+        for(i = pos - 1; i < n - 1; i++)
+        {
+            a[i] = a[i + 1];
+        }
+
+        n--;
+
+        printf("Array after deletion:\n");
+
+        for(i = 0; i < n; i++)
+        {
+            printf("%d ", a[i]);
+        }
+    }
+
+    return 0;
+}
